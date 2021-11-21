c69a06c03c794c50f507f00de4756075d2f8d981
Patrik Frankovic <patrikfrankovic6@gmail.com>
2021-11-21 01:10:29 +0100

    21.11

---
+++ DZ2/Forecast/DailyForecast.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Forecast
+{
+    public class DailyForecast
+    {
+        DateTime day;
+        Weather dailyweather;
+
+        public Weather GetDailyWeather { get { return dailyweather; } }
+        public DateTime GetDay { get { return day; } }
+
+        public Weather SetDailyWeather { set { this.dailyweather=value; } }
+        public DateTime SetDay { set { this.day=value; } }
+
+        public DailyForecast() : this(new DateTime(0), null) { }
+        public DailyForecast(DateTime day, Weather weather)
+        {
+            this.day = day;
+            this.dailyweather = weather;
+        }
+
+        public string GetAsString()
+        {
+            return $"{day.ToString()}: {dailyweather.GetAsString()}";
+        }
+    }
+}
---
+++ DZ2/Forecast/WeeklyForecast.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Forecast
+{
+    public class WeeklyForecast
+    {
+        DailyForecast[] dailyForecasts;
+
+        public WeeklyForecast(DailyForecast[] dailyForecasts)
+        {
+            this.dailyForecasts = dailyForecasts;
+        }
+
+
+        public string GetAsString()
+        {
+            string forcasts = dailyForecasts[0].GetAsString();
+            for (int i=1;i<dailyForecasts.Length;i++)
+            {
+                forcasts += $"\n{dailyForecasts[i].GetAsString()}"; 
+            }
+            return forcasts;
+        }
+
+        public double GetMaxTemperature()
+        {
+            double max = dailyForecasts[0].GetDailyWeather.GetTemperature();
+            for(int i=0;i<dailyForecasts.Length;i++)
+                if(max < dailyForecasts[i].GetDailyWeather.GetTemperature())
+                    max = dailyForecasts[i].GetDailyWeather.GetTemperature();
+            return max;
+        }
+
+        public DailyForecast this[int i]
+        {
+            get => dailyForecasts[i];
+        }
+    }
+}
